cdecc8edecb60a0b111339468b89ebf5bb33114d
lihongbo <lihongbo@iot.chinamobile.com>
2016-04-15 14:36:11 +0800

    创建NewArita项目

---
+++ Arita/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Arita
+//
+//  Created by 李宏博 on 16/4/15.
+//  Copyright © 2016年 DcBunny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NewArita
 
-## 重构阿里塔项目
+## 项目中使用的第三方库
+* 第三方库管理工具：[CocoaPods](http://www.tuicool.com/articles/7VvuAr3)
+* 网络：[Alamofire](https://github.com/Alamofire/Alamofire)
+* JSON解析：[SwiftyJSON](https://github.com/SwiftyJSON/SwiftyJSON)
+* 
