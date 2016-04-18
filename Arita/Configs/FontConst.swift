221f86917f095f4dc7b39cca2d541b90a3b8f45f
DcBunny <515665517@qq.com>
2016-04-18 10:17:52 +0800

    提交基础代码框架

---
+++ Arita/Configs/FontConst.swift
@@ -0,0 +1,18 @@
+//
+//  FontConst.swift
+//  Arita
+//
+//  Created by DcBunny on 16/4/18.
+//  Copyright © 2016年 DcBunny. All rights reserved.
+//
+
+import Foundation
+
+/**
+ *
+ *  用于放置所有字体相关的常量
+ *
+ */
+
+//MARK:- TabBar
+let kTabBarFont = 20.0
